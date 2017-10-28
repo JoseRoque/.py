e3f3fea61405e2af38b4af27f3e2d55c79b1ab86
Jose Roque <jose.roque@mavs.uta.edu>
2017-10-27 23:58:05 -0500

    Test

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
+class BSTNode:
+ def _init_ (self, key, value):
+	self.left=null;
+	self.right=null;
+	self.parent=null;
+	self.key=key;
+	self.value=value;
+
 def main():
     print("Hello World")
+    root = BSTNode(1,"a");
 
 if __name__ == "__main__":
     main()
-
