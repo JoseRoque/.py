8741059786f1170648ffea215f578fa834a390df
Jose Roque <jose.roque@mavs.uta.edu>
2017-10-28 00:23:14 -0500

    init BST

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
 class BSTNode:
- def _init_ (self, key, value):
-	self.left=null;
-	self.right=null;
-	self.parent=null;
-	self.key=key;
-	self.value=value;
+ def __init__ (self, key, value, left="", right="", parent=""):
+     self.key=key;
+     self.value=value;
+     self.left=left;
+     self.right=right;
+     self.parent=parent;
 
 def main():
     print("Hello World")
-    root = BSTNode(1,"a");
+    root = BSTNode(key=1,value="a");
 
 if __name__ == "__main__":
     main()
