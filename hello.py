30abffc72ba57cff0831fc219999048c69b280be
Jose Roque <jose.roque@mavs.uta.edu>
2017-10-28 23:44:54 -0500

    cs50 lecture

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
+import sys
+import csv
+
 class BSTNode:
  def __init__ (self, key, value, left="", right="", parent=""):
      self.key=key;
@@ -7,8 +10,31 @@ class BSTNode:
      self.parent=parent;
 
 def main():
-    print("Hello World")
+    arguments=[]
+
+    # read cmd args & print them
+    for i in range(len(sys.argv)):
+        print(sys.argv[i])
+        arguments.append(sys.argv[i])
+
+    (x,y) = (arguments[1], arguments[2])
+    print("x={}, y={}".format(x, y))
+
+    # swap them
+    (x, y) = (y, x)
+    print("x={}, y={}".format(x, y))
+
+    # print("Hello World")
     root = BSTNode(key=1,value="a");
+
+    # open/create file
+    file = open("coordinates.csv", "w")
+    writer = csv.writer(file)
+
+    # write coordinates to file
+    writer.writerow((x,y))
+
+    file.close()
 
 if __name__ == "__main__":
     main()
